c08bf4cc13843b37271a62d4e64c8c0b3e4aa225
wacton <waacton@gmail.com>
2016-05-13 08:57:46 +0100

    Extension method to append to IEnumerable

--- Tovarisch/Collections/CollectionExtensions.cs
+++ Tovarisch/Collections/CollectionExtensions.cs
@@ -2,7 +2,6 @@
 {
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
 
     public static class CollectionExtensions
     {
@@ -11,13 +10,30 @@
             return new List<T> { item };
         }
 
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> collection, params T[] items)
+        {
+            return collection.Concat(items);
+        }
+
+        public static void AddOrReplace<TK, TV>(this Dictionary<TK, TV> dictionary, TK key, TV value)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                dictionary[key] = value;
+            }
+            else
+            {
+                dictionary.Add(key, value);
+            }
+        }
+
         public static List<T> Duplicates<T>(this IEnumerable<T> collection)
         {
             return collection.GroupBy(item => item)
                              .Where(group => group.Count() > 1)
                              .Select(group => group.Key)
                              .ToList();
-        } 
+        }
 
         public static string ToDelimitedString<T>(this IEnumerable<T> collection, string delimiter)
         {
