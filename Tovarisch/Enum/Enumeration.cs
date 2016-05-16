e9f258e3502b80cd7ead2d4e8f1fa248338e0711
wacton <waacton@gmail.com>
2016-05-16 13:42:45 +0100

    Enumeration.GetAll improvement

--- Tovarisch/Enum/Enumeration.cs
+++ Tovarisch/Enum/Enumeration.cs
@@ -18,8 +18,12 @@
 
         public static IEnumerable<T> GetAll<T>() where T : Enumeration
         {
-            var fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly);
-            return fields.Select(f => f.GetValue(null)).Cast<T>();
+            var type = typeof(T);
+            var enumerationFields =
+                type.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                    .Where(field => field.FieldType == type);
+
+            return enumerationFields.Select(f => f.GetValue(null)).Cast<T>();
         }
 
         public override bool Equals(object obj)
